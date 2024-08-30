8456c057a5500bcb809ca6d24294ed225ba57d0b
Mariana8595 <a.marianavazquez@gmail.com>
2024-08-29 19:54:11 -0600

    Portfolio 2 (enlaces agregados)

--- main.js
+++ main.js
@@ -15,3 +15,35 @@ document.addEventListener('DOMContentLoaded', () => {
     // Inicializar el estado
     toggleSwitch.classList.add('off');
 })
+//mail js
+
+const btn = document.getElementById('button');
+const form = document.getElementById('form');
+const envelopeFront = document.querySelector('.envelope.front');
+const envelopeBack = document.querySelector('.envelope.back');
+const resultMessage = document.querySelector('.result-message');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    btn.value = 'Sending...';
+
+    const serviceID = 'default_service';
+    const templateID = 'template_ioyg4kp';
+
+    emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+            btn.value = 'Send Email';
+            alert('Sent!');
+
+            // Añadir clase 'sent' para activar animaciones
+            form.classList.add('sent');
+
+            // Opcional: Hacer visible el mensaje de resultado
+            resultMessage.style.opacity = 1;
+        }, (err) => {
+            btn.value = 'Send Email';
+            alert(JSON.stringify(err));
+        });
+});
+
